0defaef5c79fd17162b9fe438cd30c2f677d4b89
giangdaika <saivnct@gmail.com>
2016-04-24 08:43:41 +0700

    update repository

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -17,7 +17,7 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("Giangdaika's application is ready."));
+        return ok(index.render("Giangdaika's application is ready. hahaha"));
     }
 
 }
